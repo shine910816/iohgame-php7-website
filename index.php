8bf69b9204a27a384aeb7db677f0b79adbf95237
Kinsama <shine910816@163.com>
2020-07-20 23:25:06 +0800

    add files

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
+phpinfo();
+
 define("SRC_PATH", str_replace("\\", "/", __DIR__));
 date_default_timezone_set("Asia/Shanghai");
 mb_internal_encoding("UTF-8");
-require_once SRC_PATH . "/lib/ErrorBase.php";
-require_once SRC_PATH . "/lib/Init.php";
-
-use Ioh\Library\ErrorBase;
-$err = ErrorBase::getInstance();
-$err->raiseError("1001", "qweq");
-$err->setPos(__FILE__, __LINE__);
-$err->writeLog();
 ?>
